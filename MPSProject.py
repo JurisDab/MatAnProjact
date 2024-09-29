b3d82059762769accc3bd0d1de7bcb5160ed5734
Artūras <arturaspoliksa@gmail.com>
2024-09-29 18:13:57 +0300

    Display zeros

--- MPSProject.py
+++ MPSProject.py
@@ -1,4 +1,6 @@
 import cmath 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def f(z, c):
     return z**3 - 2*z + c
@@ -43,6 +45,36 @@ def main():
             print(f"Zero: {zero}")
     else:
         print("No zeros found.")
+
+    if real_zeros:
+        xmin, xmax, ymin, ymax = -5, 5, -5, 5
+        ticks_frequency = 1
+
+        real_zeros_array = np.array(list(real_zeros))
+        zeros_array = np.zeros_like(real_zeros_array)
+        
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.scatter(real_zeros_array, zeros_array, c='r')
+
+        ax.set(xlim=(xmin-1, xmax+1), ylim=(ymin-1, ymax+1), aspect='equal')
+
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['left'].set_position('zero')
+
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+        ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
+        ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+
+        x_ticks = np.arange(xmin, xmax+1, ticks_frequency)
+        y_ticks = np.arange(ymin, ymax+1, ticks_frequency)
+        ax.set_xticks(x_ticks[x_ticks != 0])
+        ax.set_yticks(y_ticks[y_ticks != 0])
+
+        ax.grid(which='major', color='grey', linewidth=1, linestyle='-', alpha=0.2)
+
+        plt.show()
 
 if __name__ == "__main__":
     main()
